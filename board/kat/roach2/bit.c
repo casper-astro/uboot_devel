2b08909ac141ad98f302022cfab15915688be444
David George <respectmyprivacy0@gmail.com>
2011-02-28 11:14:31 +0200

    added QDR memory tests
    Added QDR calibration and memory test to bit.c

--- board/kat/roach2/bit.c
+++ board/kat/roach2/bit.c
@@ -82,18 +82,6 @@ int bit_zdok(int which)
     zdok_set_bit(which, i, BSP_GPIO_INPUT, BSP_GPIO_VAL_1);
   }
 
-  printf("I0: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x00)));
-  printf("I1: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x10)));
-  printf("I2: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x20)));
-
-  printf("O0: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x04)));
-  printf("O1: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x14)));
-  printf("O2: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x24)));
-
-  printf("E0: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x08)));
-  printf("E1: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x18)));
-  printf("E2: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x28)));
-
   for (i=0; i < 80; i++){
     /* bit on which the expected activity would be */
     expecting = (i/10)%2 ? i - 10: i + 10;
@@ -126,26 +114,415 @@ int bit_zdok(int which)
 
   }
 
-  printf("I0: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x00)));
-  printf("I1: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x10)));
-  printf("I2: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x20)));
+  return ret;
+}
 
-  printf("O0: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x04)));
-  printf("O1: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x14)));
-  printf("O2: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x24)));
+/*********** QDR calibration *************/
 
-  printf("E0: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x08)));
-  printf("E1: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x18)));
-  printf("E2: %08x\n", *((volatile u32 *)(CONFIG_SYS_FPGA_BASE + BSP_ZDOK0_OFFSET + 0x28)));
+void qdr_set_reg(int which, u32 reg, u32 val)
+{
+  u32 offset = CONFIG_SYS_FPGA_BASE;
+  switch (which){
+     case 0:
+       offset += BSP_QDR0CONF_OFFSET;
+       break;
+     case 1:
+       offset += BSP_QDR1CONF_OFFSET;
+       break;
+     case 2:
+       offset += BSP_QDR2CONF_OFFSET;
+       break;
+     default:
+       offset += BSP_QDR3CONF_OFFSET;
+  }
+  offset += reg;
+  *((volatile u32 *) (offset)) = val;
+}
+
+u32 qdr_get_reg(int which, u32 reg)
+{
+  u32 offset = CONFIG_SYS_FPGA_BASE;
+  switch (which){
+     case 0:
+       offset += BSP_QDR0CONF_OFFSET;
+       break;
+     case 1:
+       offset += BSP_QDR1CONF_OFFSET;
+       break;
+     case 2:
+       offset += BSP_QDR2CONF_OFFSET;
+       break;
+     default:
+       offset += BSP_QDR3CONF_OFFSET;
+  }
+  offset += reg;
+  return *((volatile u32 *) (offset));
+}
+
+void qdr_dll_reset(int which)
+{
+  u32 val;
+  val = qdr_get_reg(which, BSP_QDRCONF_REG_CTRL);
+  qdr_set_reg(which, BSP_QDRCONF_REG_CTRL,
+    BSP_QDRCONF_DLL_RESET);
+  udelay(1000);
+  qdr_set_reg(which, BSP_QDRCONF_REG_CTRL,
+    BSP_QDRCONF_DLL_RUN);
+  udelay(1000);
+}
+
+int qdr_phy_tick_dly(int which, int dir)
+{
+  int i, ret=0;
+  u32 val;
+  qdr_set_reg(which, BSP_QDRCONF_REG_BITCTRL, BSP_QDRCONF_DLY_EN |
+                           (dir ? BSP_QDRCONF_DLY_INC : BSP_QDRCONF_DLY_DEC));
+  
+  for (i=0; i < 1000; i++){
+    val = qdr_get_reg(which, BSP_QDRCONF_REG_STATUS);
+    if (val & BSP_QDRCONF_DAT_RDY)
+      break;
+
+    if (i==999) {
+      printf("error: qdr data sampled done stuck low\n");
+      ret = 1;
+    }
+  }
+  return ret;
+}
+
+#define STABLE_THRESHOLD 4
+
+#define EDGE_CLEARANCE 9
+
+int qdr_align_bit(int which, int bit)
+{
+  int i;
+  u32 dreg;
+  int val;
+  int is_stable;
+  int ret=0;
+
+  u8 bit_line[32];
+  u8 stable_count = 0;
+  u8 baddies = 0;
+  int first_val = 0;
+  int prev_val = 0;
+
+  int cal_pos = EDGE_CLEARANCE;
+
+  qdr_set_reg(which, BSP_QDRCONF_REG_BITINDEX, (u32)bit);
+  qdr_set_reg(which, BSP_QDRCONF_REG_BITCTRL, BSP_QDRCONF_DLY_RST);
+  udelay(10);
+
+  /* ensure that the status register is valid */
+  qdr_phy_tick_dly(which, 1);
+  qdr_phy_tick_dly(which, 0);
+
+  for (i=0; i < 32; i++) {
+    dreg = qdr_get_reg(which, BSP_QDRCONF_REG_STATUS);
+    is_stable = dreg & 0x00000100;
+    val = dreg % 4;
+    bit_line[i] = is_stable ? val : 0xb;
+
+    if (is_stable && (val == 1 || val == 2)){
+      if (!first_val){
+        first_val = val;
+      }
+      stable_count++;
+
+      if (val != prev_val)
+        stable_count = 0;
+         
+      if (first_val != val && stable_count >= STABLE_THRESHOLD){
+        if (i + EDGE_CLEARANCE - STABLE_THRESHOLD >= 31){
+          cal_pos = i - baddies - STABLE_THRESHOLD - EDGE_CLEARANCE;
+          if (cal_pos < EDGE_CLEARANCE){
+            printf("warning: bit %d calibation point %d may not be safe\n", bit, cal_pos);
+          }
+          break;
+        } else {
+          cal_pos = i + EDGE_CLEARANCE - STABLE_THRESHOLD;
+          if (i >= 32){
+            ret = 1;
+            printf("error: bit %d calibration point %d over the edge\n", bit, cal_pos);
+            cal_pos = EDGE_CLEARANCE;
+          }
+          break;
+        }
+      }
+    } else {
+      if (first_val){
+        baddies++;
+      }
+      stable_count = 0;
+    }
+    prev_val = val;
+
+    qdr_phy_tick_dly(which, 1);
+    if (i==31){
+      printf("warning: no edge detected, using default cal_pos\n");
+      printf("b = %d, g = %d, f = %d\n", baddies, stable_count, first_val);
+      baddies = 100;
+    }
+  }
+  if (baddies == 100){
+    for (i=0;i < 32; i++){
+      printf("%x",bit_line[i]);
+    }
+    printf("\n");
+  }
+#ifdef DEBUG
+  printf(" cal pos = %d, ", cal_pos);
+#endif
+  qdr_set_reg(which, BSP_QDRCONF_REG_BITCTRL, BSP_QDRCONF_DLY_RST);
+  udelay(10);
+  for (i=0; i < cal_pos; i++) {
+    qdr_phy_tick_dly(which, 1);
+  }
+
+  /* now get half bit alignment */
+  udelay(10);
+  dreg = qdr_get_reg(which, BSP_QDRCONF_REG_STATUS);
+  is_stable = dreg & 0x00000100;
+  val = dreg % 4;
+  if (val == 1){
+    qdr_set_reg(which, BSP_QDRCONF_REG_BITCTRL, BSP_QDRCONF_ALIGN_EN);
+#ifdef DEBUG
+    printf("align used\n");
+#endif
+  } else {
+#ifdef DEBUG
+    printf("align bypass\n");
+#endif
+  }
+  if (!is_stable || val == 0 || val == 3){
+    printf("error: apparent error in calibration\n");
+    ret = 1;
+  }
+  return ret;
+}
+
+void qdr_dump_bit(int which, int bit)
+{
+  int i;
+  u32 dreg;
+  int is_stable;
+  int val;
+  qdr_set_reg(which, BSP_QDRCONF_REG_BITINDEX, (u32)bit);
+  qdr_set_reg(which, BSP_QDRCONF_REG_BITCTRL, BSP_QDRCONF_DLY_RST);
+
+  /* ensure that the status register is valid */
+  qdr_phy_tick_dly(which, 1);
+  qdr_phy_tick_dly(which, 0);
+
+  printf("%2d: ", bit);
+  for (i=0; i < 32; i++) {
+    dreg = qdr_get_reg(which, BSP_QDRCONF_REG_STATUS);
+    is_stable = dreg & 0x00000100;
+    val = dreg % 4;
+
+    if (is_stable) {
+      switch (val){
+        case 1:
+          printf("1");
+          break;
+        case 2:
+          printf("2");
+          break;
+        default:
+          printf("e");
+      }
+    } else {
+      printf("x");
+    }
+    /* increase delay by one tap */
+    qdr_phy_tick_dly(which, 1);
+  }
+  //printf("\n");
+}
+
+int qdr_phy_cal(int which)
+{
+  int i;
+  int ret=0;
+  u32 dreg;
+
+  qdr_dll_reset(which);
+
+  qdr_set_reg(which, BSP_QDRCONF_REG_CTRL, BSP_QDRCONF_CAL_EN | BSP_QDRCONF_DLL_RUN);
+
+  for (i=0; i < 1000; i++){
+    dreg = qdr_get_reg(which, BSP_QDRCONF_REG_STATUS);
+    if (dreg & BSP_QDRCONF_CAL_RDY)
+      break;
+      
+    if (i==999){
+      printf("error: cal state machine not ready\n");
+    }
+  }
+
+  for (i=0; i < 36; i++) {
+#ifdef DEBUG
+    qdr_dump_bit(which, i);
+#endif
+    ret = ret | qdr_align_bit(which, i);
+  }
+
+  qdr_set_reg(which, BSP_QDRCONF_REG_CTRL, BSP_QDRCONF_DLL_RUN);
+
+  return ret;
+}
+
+/*********** QDR memory testing *************/
+
+void qdr_mem_set_reg(int which, u32 reg, u32 val)
+{
+  u32 offset = CONFIG_SYS_FPGA_BASE;
+  switch (which){
+     case 0:
+       offset += BSP_QDR0_OFFSET;
+       break;
+     case 1:
+       offset += BSP_QDR1_OFFSET;
+       break;
+     case 2:
+       offset += BSP_QDR2_OFFSET;
+       break;
+     default:
+       offset += BSP_QDR3_OFFSET;
+  }
+  offset += reg;
+  *((volatile u32 *) (offset)) = val;
+}
+
+u32 qdr_mem_get_reg(int which, u32 reg)
+{
+  u32 offset = CONFIG_SYS_FPGA_BASE;
+  switch (which){
+     case 0:
+       offset += BSP_QDR0_OFFSET;
+       break;
+     case 1:
+       offset += BSP_QDR1_OFFSET;
+       break;
+     case 2:
+       offset += BSP_QDR2_OFFSET;
+       break;
+     default:
+       offset += BSP_QDR3_OFFSET;
+  }
+  offset += reg;
+  return *((volatile u32 *) (offset));
+}
+
+int qdr_mem_test(int which)
+{
+  int ret=0;
+  u32 i, j;
+  u32 d, t;
+  d = qdr_mem_get_reg(which, BSP_QDRM_REG_STAT);
+
+  if (!(d & BSP_QDRM_PHYRDY))
+    printf("warning: qdr phy not ready for mem test\n");
+
+  if ((d & BSP_QDRM_CALFAIL))
+    printf("warning: qdr mem test with cal failed\n");
+
+  for (i=0; i < BSP_QDRM_DEPTH; i++){
+    qdr_mem_set_reg(which, BSP_QDRM_REG_A, i);
+
+    d = (i % 2) ? (~(i / 2)) : (i/2);
+
+    for (j=0; j < BSP_QDRM_DLEN; j++){
+      qdr_mem_set_reg(which, BSP_QDRM_REG_D + (j << 2), d);
+    }
+    qdr_mem_set_reg(which, BSP_QDRM_REG_CTRL, BSP_QDRM_WREN);
+  }
+
+  for (i=0; i < BSP_QDRM_DEPTH; i++){
+    qdr_mem_set_reg(which, BSP_QDRM_REG_A, i);
+
+    qdr_mem_set_reg(which, BSP_QDRM_REG_CTRL, BSP_QDRM_RDEN);
+    /* wait for rd flag to clear */
+    for (j=0; j < 1000; j++){
+      if (!(qdr_mem_get_reg(which, BSP_QDRM_REG_CTRL) & BSP_QDRM_RDEN)){
+        break;
+      }
+      if (j == 999) {
+         printf("warning: timeout waiting for qdr read completion\n");
+      }
+    }
+    
+    for (j=0; j < BSP_QDRM_DLEN; j++){
+      t = qdr_mem_get_reg(which, BSP_QDRM_REG_Q + (j << 2));
+#ifdef DEBUG
+      printf("%8x[%d]: %08x\n",i,j,t);
+#endif
+      d = (i % 2) ? (~(i / 2)) : (i/2);
+      if (j == 0)
+        d &= 0xffff;
+
+      if (t != d){
+        ret = 1;
+        printf("FAIL: QDR%d, data mismatch: addr = %x, offset=%d, got=%x, expected=%x\n", which, i, j, t, d);
+      }
+    }
+    if (ret)
+      break;
+  }
+  if (!ret){
+    printf("PASSED: QDR%d memory test\n", which);
+  }
+
+  return ret;
+}
+
+int qdr_fabric_test(int which)
+{
+  printf("warning: QDR fabric test not implemented\n");
+  return 0;
+}
+
+int bit_qdr(int which)
+{
+  int ret = 0;
+  if ((ret |= qdr_phy_cal(which))) {
+    printf("error: QDR-%d calibration failed\n", which);
+  }
+
+  if ((ret |= qdr_mem_test(which))) {
+    printf("error: QDR-%d cpu memory test failed\n", which);
+  }
+
+  if ((ret |= qdr_fabric_test(which))) {
+    printf("error: QDR-%d fabric test failed\n", which);
+  }
+
   return ret;
 }
 
 
-
 static int do_roach2_test(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
-  bit_zdok(0);
-  bit_zdok(1);
+  /*
+  if (bit_zdok(0)) {
+    printf("FAIL: ZDOK0 test failed\n");
+  } else {
+    printf("PASS: ZDOK1 test passed\n");
+  }
+  if (bit_zdok(1)) {
+    printf("FAIL: ZDOK0 test failed\n");
+  } else {
+    printf("PASS: ZDOK1 test passed\n");
+  }
+  */
+
+  bit_qdr(0);
+  bit_qdr(1);
+  bit_qdr(2);
+  bit_qdr(3);
+
   return 0;
 }
 
